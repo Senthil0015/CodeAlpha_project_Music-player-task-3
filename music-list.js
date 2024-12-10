24f579e106bcd3dce9951bba9544ebecc9213208
Senthil0015 <sandy15cr7@gmail.com>
2024-12-10 23:18:43 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,47 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+let allMusic = [
+  {
+    name: "Popular",
+    artist: "The Weekend,Carti, Madonna",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Way Down We Go",
+    artist: "KALEO",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Unholy(feat.Kim Petras)",
+    artist: "Sam Smith, Kim Petras",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Kill Bill",
+    artist: "SZA",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Formula",
+    artist: "Labrinth",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "double take",
+    artist: "Dhruv",
+    img: "music-6",
+    src: "music-6",
+  },
+];
